1907113e84d89268d09392c3b9ed81e5fcc4c08a
PTS39 <thivakorn2539@gmail.com>
2024-08-05 20:35:05 +0700

    up

--- login.js
+++ login.js
@@ -1,17 +1,3 @@
-// การตั้งค่า Firebase ของคุณ
-var firebaseConfig = {
-    apiKey: "AIzaSyD3zU3vffz1W8YBbtCHbgLUrd8fYRlBFXU",
-    authDomain: "addtime-453e4.firebaseapp.com",
-    databaseURL: "https://addtime-453e4-default-rtdb.firebaseio.com",
-    projectId: "addtime-453e4",
-    storageBucket: "addtime-453e4.appspot.com",
-    messagingSenderId: "790574221214",
-    appId: "1:790574221214:web:f6cfa78f1cd46cfad1f88f",
-    measurementId: "G-1EPHN50YG0"
-};
-// เริ่มต้น Firebase
-firebase.initializeApp(firebaseConfig);
-
 // ฟังก์ชั่นการล็อกอิน
 document.getElementById('loginForm').addEventListener('submit', function (e) {
     e.preventDefault();
