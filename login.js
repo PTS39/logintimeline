bb531bda4d552bc856ec4bf8df48a6343d4d9a4a
PTS39 <thivakorn2539@gmail.com>
2024-08-07 17:07:52 +0700

    up

--- login.js
+++ login.js
@@ -1,28 +1,3 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-
-    var data = {
-        username: username,
-        password: password,
-        action: 'login'
-    };
-
-    fetch('https://script.google.com/macros/s/AKfycbzuDLDgqdPEf7Hfu1WZZeiGb52a75R6Y0eDsjtLA1ISJ9tDjMS8PsTAsS521OLP_dpURg/exec', {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(response => response.text())
-    .then(result => {
-        console.log(result);
-        alert('Login successful!');
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('Login failed!');
-    });
-});
+function openLink(url) {
+    window.open(url, '_blank');
+}
